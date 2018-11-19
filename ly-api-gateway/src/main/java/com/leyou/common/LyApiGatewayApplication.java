53935e44571272762c8c1d0324e2eb31cb07b1d5
邵益炯 <shaoyijiong@worken.cn>
2018-11-19 19:21:05 +0800

    feat(新建model):网关与注册中心

---
+++ ly-api-gateway/src/main/java/com/leyou/common/LyApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.leyou.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableZuulProxy
+public class LyApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LyApiGatewayApplication.class, args);
+  }
+}
---
+++ ly-api-gateway/src/test/java/com/leyou/common/LyApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leyou.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LyApiGatewayApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
