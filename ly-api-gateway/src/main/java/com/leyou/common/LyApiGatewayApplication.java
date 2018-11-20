a95629ee21410cf0226b75ff5dd4fc98c6fd43a1
邵益炯 <shaoyijiong@worken.cn>
2018-11-20 19:16:13 +0800

    feat(商品):新建商品条目

--- ly-api-gateway/src/main/java/com/leyou/common/LyApiGatewayApplication.java
+++ ly-api-gateway/src/main/java/com/leyou/common/LyApiGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @author shaoyijoing
+ */
 @SpringCloudApplication
 @EnableZuulProxy
 public class LyApiGatewayApplication {
---
+++ ly-item/src/test/java/com/leyou/LyItemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leyou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LyItemApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
