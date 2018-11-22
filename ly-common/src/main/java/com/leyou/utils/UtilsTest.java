55d8bf56c08a7d681723fb2c87281fd575a87641
邵益炯 <shaoyijiong@worken.cn>
2018-11-22 19:09:57 +0800

    feat(工具类):新建工具项目

--- ly-common/src/main/java/com/leyou/utils/UtilsTest.java
+++ ly-common/src/main/java/com/leyou/utils/UtilsTest.java
@@ -1,14 +1,43 @@
 package com.leyou.utils;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
 /**
  * .
+ *
  * @author 邵益炯
  * @date 2018/11/21
  */
 public class UtilsTest {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
 
+    LinkedBlockingDeque<Long> longs = new LinkedBlockingDeque<>();
+
+    //机器中心 数据中心
+    IdWorker idWorker = new IdWorker(10L, 10L);
+    long l = idWorker.nextId();
+    ExecutorService service = Executors.newFixedThreadPool(1000);
+    for (int j = 0; j < 1000; j++) {
+      service.submit(() -> {
+        for (int i = 0; i < 1000; i++) {
+          long a = idWorker.nextId();
+          longs.add(a);
+          System.out.println(idWorker.nextId());
+        }
+      });
+    }
+    TimeUnit.SECONDS.sleep(10);
+
+    Set<Long> set = new HashSet<>(1_000_000);
+    set.addAll(longs);
+    System.out.println(set.size());
+    service.shutdown();
   }
 
 }
