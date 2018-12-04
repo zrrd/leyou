718c86d0dc81dc81a96786fba7f93a1a1883a13a
邵益炯 <shaoyijiong@worken.cn>
2018-12-04 17:51:54 +0800

    feat(brand):品牌保存3 完

--- ly-upload/src/main/java/com/leyou/controller/UploadController.java
+++ ly-upload/src/main/java/com/leyou/controller/UploadController.java
@@ -1,6 +1,11 @@
 package com.leyou.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,12 +15,26 @@ import org.springframework.web.multipart.MultipartFile;
  * @author 邵益炯
  * @date 2018/12/4
  */
+@Slf4j
 @RestController
 @RequestMapping("upload")
 public class UploadController {
 
+  private static final List<String> SUFFIXES = Arrays.asList("image/png", "image/jpeg");
+
+  /**
+   * 上次图片文件.
+   */
   @RequestMapping("image")
-  public String upload(MultipartFile file) {
+  public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+    //1.文件校验
+    //1.1 校验文件类型
+    String type = file.getContentType();
+    if (!SUFFIXES.contains(type)) {
+      throw new RuntimeException("上传失败，文件类型不匹配");
+    }
+    //1.2 校验文件数据内容
+    //文件保存
     return null;
   }
 }
