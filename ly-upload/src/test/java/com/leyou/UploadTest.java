b0ec7b4a4023a67195b022d429cce6b0dd19fe17
邵益炯 <shaoyijiong@worken.cn>
2018-12-06 18:17:52 +0800

    feat(upload):文件上传

--- ly-upload/src/test/java/com/leyou/UploadTest.java
+++ ly-upload/src/test/java/com/leyou/UploadTest.java
@@ -42,7 +42,7 @@ public class UploadTest {
 
   @Test
   public void testUploadAndCreateThumb() throws FileNotFoundException {
-    File file = new File("D:\\test\\baby.png");
+    File file = new File("F:\\image\\aaa.jpg");
     // 上传并且生成缩略图
     StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
         new FileInputStream(file), file.length(), "png", null);
