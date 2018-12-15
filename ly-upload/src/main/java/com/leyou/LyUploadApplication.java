7d4e9a2ca34c63d200b72db9bbdc427b9cde3cc4
shaoyijiong <fantasy9417@163.com>
2018-12-15 14:21:32 +0800

    测试

--- ly-upload/src/main/java/com/leyou/LyUploadApplication.java
+++ ly-upload/src/main/java/com/leyou/LyUploadApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * .
+ * 测试git.
  *
  * @author shaoyijiong
  */
