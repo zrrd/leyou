a745367b8cef876ca14483f6fd24d1bad21dcbc4
邵益炯 <shaoyijiong@worken.cn>
2019-01-07 13:26:47 +0800

    feat(商品详情页): 新建项目

---
+++ ly-goods-web/src/main/java/com/leyou/goods/lygoodsweb/LyGoodsWebApplication.java
@@ -0,0 +1,14 @@
+package com.leyou.goods.lygoodsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyGoodsWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LyGoodsWebApplication.class, args);
+  }
+
+}
+
